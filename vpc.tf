ba5e43b204fa9e3dc12c5687d075e03ac1f3520c
matthews <cnusrinivasu@gmail.com>
2024-11-06 23:31:27 +0530

    mysql module used sg child module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "vpc" {
+  source                ="git::https://github.com/Git-Yellow1986/terraform-aws-vpc.git?ref=main"
+  #source               = "../terraform-aws-vpc"
+  vpc_cidr              = var.vpc_cidr
+  project_name          = var.project_n
+  environment           = var.environment_e
+  common_tags           = var.common_tagss
+  public_subnet_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
+  database_subnet_cidrs = var.database_subnet_cidrs
+  is_peering_required   = var.is_peering_required
+
+
+}
